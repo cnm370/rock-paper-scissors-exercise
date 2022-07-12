64820ee3690effc648c821d765e93a0e72df0cb8
cnm370 <cnm370@stern.nyu.edu>
2022-07-12 19:53:39 -0400

    User choice

---
+++ game.py
@@ -0,0 +1,23 @@
+
+
+print("WELCOME TO ROCK PAPER SCISSORS GAME")
+
+
+# USER INPUTS
+
+user_choice = input("Please make a selection ('rock', 'paper', 'scisoors'): ")
+
+# You chose : 'rock'
+print("You chose:", user_choice)  # SUFFICIENT!!
+print(f"You chose: '{user_choice}' ")
+
+
+
+
+# VALIDATE USER INPUTS
+
+
+# COMPUTER  CHOICE
+
+# DETERMINE THE WINNER
+
