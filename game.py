ea9f7cee0c7d620850189d14cb6ce8b98dc33ff1
cnm370 <cnm370@stern.nyu.edu>
2022-07-12 20:30:56 -0400

    Random Computer Choice

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 
+import random
+
+
 
 print("WELCOME TO ROCK PAPER SCISSORS GAME")
 
@@ -12,12 +15,28 @@ print("You chose:", user_choice)  # SUFFICIENT!!
 print(f"You chose: '{user_choice}' ")
 
 
-
-
 # VALIDATE USER INPUTS
+
+
+
+
 
 
 # COMPUTER  CHOICE
 
+
+# IMPORT RANDOM
+
+valid_options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(valid_options)
+print("Computer chose:", computer_choice)
+
+
+
 # DETERMINE THE WINNER
 
+
+
+# DISPLAY RESULTS
+#
+#--------------------------------
