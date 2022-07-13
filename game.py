b47e7a1b85601fdeb388704883f9d0e85ceff9c1
cnm370 <cnm370@stern.nyu.edu>
2022-07-12 20:47:03 -0400

    Added winner determination

--- game.py
+++ game.py
@@ -18,10 +18,6 @@ print(f"You chose: '{user_choice}' ")
 # VALIDATE USER INPUTS
 
 
-
-
-
-
 # COMPUTER  CHOICE
 
 
@@ -32,10 +28,28 @@ computer_choice = random.choice(valid_options)
 print("Computer chose:", computer_choice)
 
 
-
 # DETERMINE THE WINNER
 
+# adapted from code shared in slack by Bonnie
+# https://nyu-tech-2335.slack.com/archives/C5WPFSB52/p1657672686150239
 
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print("Rock crushes scissors. You win!")
+    else:
+        print("Paper covers rock. You lose.")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Paper covers rock. You win!")
+    else:
+        print("Scissors cuts paper. You lose.")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print("Scissors cuts paper. You win!")
+    else:
+        print("Rock crushes scissors. You lose.")
 
 # DISPLAY RESULTS
 #
